4dc2e0c012d8715cd9960e0ac8895c68b11943bb
israelone <itzisraelone@gmail.com>
2021-12-04 11:18:45 -0600

    Revert "Nodejs installation"

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchAppBar from './components/navbar/navbar';
+
+function App() {
+  return (
+    <SearchAppBar></SearchAppBar>
+  );
+}
+
+export default App;
