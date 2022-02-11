ea187278bcb316cde9e24a3d2c2052c39d16d007
IzzyDev <itzisraelone@gmail.com>
2022-02-11 16:31:55 -0600

    Added axios implementation

--- FrontEnd/src/components/card-container/cards-container.js
+++ FrontEnd/src/components/card-container/cards-container.js
@@ -3,19 +3,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 100%;
-  background-color: rgba(255, 93, 0, 0.85);
-  width: fit-content;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
+height: fit-content;
+background-color: rgba(255,93,0,0.85);
+width: fit-content;
+display: flex;
+flex-direction: column;
+align-items: center;
+}
 `;
 
-const CardsContainer = () => {
+const CardsContainer = (props) => {
+   //Temp fix maybe?
+  if (!Object.keys(props)[0]) return null;
+
   return (
-    <Container>
-      <ActionAreaCard></ActionAreaCard>
-      <ActionAreaCard></ActionAreaCard>
+    <Container>{Object.values(props)[0].map((item)=>{
+      return(<ActionAreaCard item={item} ></ActionAreaCard>)
+    })}
     </Container>
   );
 };
