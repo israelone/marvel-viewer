b6d43a00c24b101a9e0091265fb85d97f63c57ca
israelone <itzisraelone@gmail.com>
2022-03-10 16:43:51 -0600

    Formatted with Prettier

--- FrontEnd/src/components/card-container/card/card.js
+++ FrontEnd/src/components/card-container/card/card.js
@@ -7,20 +7,33 @@ import { CardActionArea } from "@mui/material";
 
 export default function ActionAreaCard(props) {
   return (
-    <Card sx={{ maxWidth: 345, display: "inline-block" }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+        display: "flex",
+        justifySelf: "center",
+        marginTop: "20px",
+      }}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
           height="500"
-          image={props.item.thumbnail.path+'/portrait_uncanny.'+ props.item.thumbnail.extension}
+          image={
+            props.item.thumbnail.path +
+            "/portrait_uncanny." +
+            props.item.thumbnail.extension
+          }
           alt={props.item.title}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-          {props.item.title}
+            {props.item.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {props.item.description ? props.item.description : "No Description Available"}
+            {props.item.description
+              ? props.item.description
+              : "No Description Available"}
           </Typography>
         </CardContent>
       </CardActionArea>
