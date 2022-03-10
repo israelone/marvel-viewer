05661faddfa0c42b79fb88e07c91fdf3e6c582c1
israelone <itzisraelone@gmail.com>
2022-03-10 16:44:50 -0600

    Implemented Indiana drag scroll

--- FrontEnd/src/components/card-container/cards-container.js
+++ FrontEnd/src/components/card-container/cards-container.js
@@ -1,26 +1,17 @@
 import ActionAreaCard from "./card/card";
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-height: fit-content;
-background-color: rgba(255,93,0,0.85);
-width: fit-content;
-display: flex;
-flex-direction: column;
-align-items: center;
-}
-`;
+import ScrollContainer from "react-indiana-drag-scroll";
 
 const CardsContainer = (props) => {
-   //Temp fix maybe?
+  //Temp fix maybe?
   if (!Object.keys(props)[0]) return null;
 
   return (
-    <Container>{Object.values(props)[0].map((item)=>{
-      return(<ActionAreaCard item={item} ></ActionAreaCard>)
-    })}
-    </Container>
+    <ScrollContainer id={"scroll"} horizontal={false}>
+      {Object.values(props)[0].map((item) => {
+        return <ActionAreaCard item={item}></ActionAreaCard>;
+      })}
+    </ScrollContainer>
   );
 };
 
